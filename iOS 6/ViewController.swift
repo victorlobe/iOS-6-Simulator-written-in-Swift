c16a68d20388bd34bbe0367c2355e615754c59b9
Victor Lobe <victor.lobe@me.com>
2017-07-29 16:48:17 +0200

    Initial Commit

---
+++ iOS 6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS 6
+//
+//  Created by Victor Lobe on 29.07.17.
+//  Copyright © 2017 Victor Lobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
